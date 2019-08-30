f9670b88c2c970cba2c8efbc08c3a1be2363c3dc
XXXX <>
2019-08-29 19:36:21 -0500

    Adiciona clase para leer de consola

---
+++ src/Prueba.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Full");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite :");
+        String dato = sc.nextLine();
+        System.out.println(" "+dato);
+    }
+}
